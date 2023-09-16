cfa9408c5afded92990f85dd221adc577b183a85
Jake Marchewitz <jjmarchewitz@gmail.com>
2023-09-16 10:53:02 -0400

    test

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,20 +1,54 @@
-use std::path::PathBuf;
+// use std::path::PathBuf;
 
 use dig_music::{self, GroupType};
 
-use rayon::prelude::*;
+use clap::{Parser, Subcommand, ValueEnum};
+use eyre::Result;
+// use rayon::prelude::*;
 
-fn main() {
-    let path: PathBuf = "/Users/jjmarch/Repos/dig-music/test-data/my_spotify_data_JUL_23.zip"
-        .parse()
-        .unwrap();
+mod help;
+mod spot;
 
-    let plays = dig_music::load_plays(path).unwrap();
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum StreamingService {
+    Spotify,
+    Apple,
+    Amazon,
+    Google,
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Spotify { path: String },
+    Apple { path: String },
+    Amazon { path: String },
+    Google { path: String },
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    match args.command {
+        // Commands::Help => help::help_main(),
+        Commands::Spotify { path } => spot::spotify_main(path),
+        _ => help::help_main(),
+    }
+
+    // let plays = dig_music::load_plays(args.path.into()).unwrap();
 
     // TODO: Turn this into a Vec<PlayGroup> where PlayGroup is an enum
-    let mut grouped_data = dig_music::group_plays_together(plays, GroupType::Artist);
+    // let mut grouped_data = dig_music::group_plays_together(plays, GroupType::Artist);
 
-    grouped_data.par_sort_by_key(|e| e.get_aggregated_data().ms_played.total);
+    // grouped_data.par_sort_by_key(|e| e.get_aggregated_data().ms_played.total);
 
-    dbg!(grouped_data);
+    // dbg!(grouped_data);
+
+    Ok(())
 }
---
+++ cli/src/spot.rs
@@ -0,0 +1,5 @@
+use std::path::Path;
+
+pub fn spotify_main(path: String) {
+    let p = Path::new(path.as_str());
+}
