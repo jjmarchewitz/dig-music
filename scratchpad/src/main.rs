5c3c23dd7f6834f90c8bf79059cd6eeb67123bbc
Jake Marchewitz <jjmarchewitz@gmail.com>
2024-04-01 22:23:03 -0400

    clipboard

--- scratchpad/src/main.rs
+++ scratchpad/src/main.rs
@@ -1,11 +1,15 @@
 mod scripts;
 
+use arboard::Clipboard;
 use eyre::Result;
 
 fn main() -> Result<()> {
     let playlist_str = scripts::top_intersecting::run()?;
 
-    println!("{}", playlist_str);
+    let mut clipboard = Clipboard::new()?;
+    clipboard.set_text(playlist_str)?;
+
+    println!("\n> Copied to clipboard!");
 
     Ok(())
 }
